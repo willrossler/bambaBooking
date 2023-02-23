3689fd7c6587a5fdc9bc35484fb7b584e0525229
William Rössler <71815793+willrossler@users.noreply.github.com>
2023-02-23 13:23:27 +0100

    pin

--- src/detailsComponents/MapMarker.js
+++ src/detailsComponents/MapMarker.js
@@ -11,15 +11,16 @@
 // };
 
 const MapMarker = () => {
-  const svgMarkup = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44">
-      <circle fill="#ffffff" cx="22" cy="22" r="20" stroke="#3c469c" stroke-width="4"/>
-      <circle fill="#f5aac9" cx="22" cy="22" r="12"/>
-    </svg>
-  `;
-  const svgUrl = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(
-    svgMarkup
-  )}`;
-  return <img src={svgUrl} alt="Map marker" />;
+  const pinStyle = {
+    path: "M22-48c-12.15 0-22 9.85-22 22 0 13.2 21.95 26.78 21.95 26.78s.05-.05.13-.13c.32-.4.81-.65 1.37-.65.98 0 1.77.79 1.77 1.77 0 .39-.13.75-.34 1.03-.09.11-.17.22-.25.34-.07.12-.13.25-.19.38-.04.1-.08.21-.11.32-.03.11-.06.22-.08.34-.01.07-.02.13-.03.2-.01.1-.02.2-.02.3 0 .98.79 1.77 1.77 1.77.98 0 1.77-.79 1.77-1.77 0-.1-.01-.2-.02-.3-.01-.07-.02-.13-.03-.2-.02-.12-.05-.23-.08-.34-.06-.13-.12-.26-.19-.38-.08-.12-.16-.23-.25-.34-.21-.28-.34-.64-.34-1.03 0-.98.79-1.77 1.77-1.77.56 0 1.05.25 1.37.65.08.08.13.13.13.13s21.95-13.58 21.95-26.78c0-12.15-9.85-22-22-22z",
+    fillColor: "#f5aac9",
+    fillOpacity: 1,
+    strokeColor: "#3c469c",
+    strokeWeight: 4,
+    scale: 1.5,
+  };
+
+  return <Pin icon={pinStyle} />;
 };
+
 export default MapMarker;
