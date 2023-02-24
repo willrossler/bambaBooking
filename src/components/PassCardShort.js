f86aa3bc60914730fed587da302fdb6a495d628b
William Rössler <71815793+willrossler@users.noreply.github.com>
2023-02-24 22:56:16 +0100

    zoom lvl on mobile map

---
+++ src/components/PassCardShort.js
@@ -0,0 +1,12 @@
+import React from "react";
+import blackCard from "../assets/images/Card_Black_Square.jpg";
+import whiteCard from "../assets/images/Card_White_Square.jpg";
+import ptCard from "../assets/images/Card_PT_Square.jpg";
+import giftCard from "../assets/images/Card_Klipp_Square.jpg";
+import { Row, Col, Container } from "react-bootstrap";
+
+const PassCardShort = () => {
+  return <div>PassCardShort</div>;
+};
+
+export default PassCardShort;
