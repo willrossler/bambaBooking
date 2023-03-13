a06eaec2b85a0f376b10620b50b0d6a64ae26a7d
William Rössler <71815793+willrossler@users.noreply.github.com>
2023-03-13 15:22:17 +0100

    footer merge

---
+++ src/components/MobileFooter.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import bLogo from "../assets/logos/bLogo.svg";
+import "../componentStyles/mobileFooter.css";
+import FooterLinks from "./FooterLinks";
+import ContactInfo from "./ContactInfo";
+import { Link, useLocation } from "react-router-dom";
+
+const MobileFooter = () => {
+  const location = useLocation();
+  const [footerColor, setfooterColor] = useState("black");
+
+  useEffect(() => {
+    if (location.pathname === "/booking") {
+      setfooterColor("#000000");
+    } else {
+      setfooterColor("#3C469C");
+    }
+  }, [location.pathname]);
+
+  return (
+    <>
+      <div
+        class="mobileFooter parent"
+        style={{
+          backgroundColor: footerColor,
+          marginTop: "40px",
+          paddingTop: "60px",
+        }}
+      >
+        <div class="div1 container px-4">
+          <img src={bLogo} style={{ width: "70px", height: "70px" }} />
+        </div>
+        <div style={{ marginBottom: "40px" }} class="div2">
+          <FooterLinks />
+        </div>
+        <div class="div3 ">
+          <ContactInfo />
+        </div>
+      </div>
+    </>
+  );
+};
+export default MobileFooter;
